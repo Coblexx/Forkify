963db03ec94d56c79d223be0302b6f368537ba57
radek <radek.glogowski3@gmail.com>
2023-08-07 18:35:30 +0200

    Refactorinig for MVC

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,9 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -11,3 +17,25 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // loading recipe
+    await model.loadRecipe(id);
+    // rendering recipe
+    recipeView.render(model.state.recipe);
+
+    // console.log(recipe);
+  } catch (err) {
+    console.error(err);
+  }
+};
+controlRecipes();
+
+['hashchange', 'load'].forEach(event =>
+  window.addEventListener(event, controlRecipes)
+);
