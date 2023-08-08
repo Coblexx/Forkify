ddbe7f161c6d8d8be41aec9faf67d556ef3de2fd
radek <radek.glogowski3@gmail.com>
2023-08-08 23:48:05 +0200

    added config.js

---
+++ src/js/config.js
@@ -0,0 +1,3 @@
+//constant variables that should be used across the project, only the important ones, responsible for defining the app itself
+
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes`;
