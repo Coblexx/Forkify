c030ad5dc7ac425917b9890c7f9482636c14bb9e
radek <radek.glogowski3@gmail.com>
2023-08-09 00:06:14 +0200

    added config.js, helpers.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,4 @@
 //constant variables that should be used across the project, only the important ones, responsible for defining the app itself
 
 export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes`;
+export const TIMEOUT_SEC = 10;
--- src/js/model.js
+++ src/js/model.js
@@ -1,4 +1,5 @@
 import { API_URL } from './config.js';
+import { getJSON } from './helpers.js';
 
 export const state = {
   recipe: {},
@@ -6,11 +7,7 @@ export const state = {
 
 export const loadRecipe = async function (id) {
   try {
-    const res = await fetch(`${API_URL}/${id}`);
-    const data = await res.json();
-    // console.log(data);
-
-    if (!res.ok) throw new Error(`${data.message}, ${res.status}`);
+    const data = await getJSON(`${API_URL}/${id}`);
 
     const { recipe } = data.data;
 
@@ -27,6 +24,6 @@ export const loadRecipe = async function (id) {
 
     // console.log(state.recipe);
   } catch (err) {
-    console.error(err);
+    console.error(`🐝${err}🐝 `);
   }
 };
