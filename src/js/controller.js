f1fa3b6602d296eb10a07eb3ab92d23f3d728ac8
radek <radek.glogowski3@gmail.com>
2023-08-10 00:53:46 +0200

    added multiple views, added View parent class

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,16 +3,16 @@ import 'regenerator-runtime/runtime';
 
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
-
-const recipeContainer = document.querySelector('.recipe');
-
-// https://forkify-api.herokuapp.com/v2
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+// import { TIMEOUT_SEC } from './config.js';
 
 ///////////////////////////////////////
 
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
+
     if (!id) return;
     recipeView.renderSpinner();
 
@@ -28,7 +28,24 @@ const controlRecipes = async function () {
 };
 controlRecipes();
 
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    await model.loadSearchResults(query);
+
+    resultsView.render(model.state.search.results);
+  } catch (error) {
+    console.error(error);
+  }
+};
+controlSearchResults();
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 init();
